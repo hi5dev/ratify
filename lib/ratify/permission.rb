7cdffd3a63c7e3df399fa82d608bfa032faae388
Travis Haynes <travis@hi5dev.com>
2018-03-26 16:25:49 -0700

    more documentation

--- lib/ratify/permission.rb
+++ lib/ratify/permission.rb
@@ -1,12 +1,21 @@
+# @!visibility private
 class Permission
   attr_reader :object, :actions, :conditions
 
+  # @param [Object] object
+  # @param [*Symbol] actions
+  # @param [**Hash] conditions
+  # @return [Permission] New instance of {Permission}.
   def initialize(object, *actions, **conditions)
     @object = object
     @actions = actions
     @conditions = conditions
   end
 
+  # @param [Object] object
+  # @param [*Symbol] actions
+  # @param [Object] scope
+  # @return [true | false] Whether or not the object is permitted.
   def permits?(object, *actions, scope: nil)
     object_matches?(object) &&
     action_matches?(actions) &&
@@ -15,10 +24,18 @@ class Permission
 
   private
 
+  # @param [Array<Symbol>] actions
+  # @return [true | false] If the given actions are all in the permission.
   def action_matches?(actions)
     actions.all? { |action| self.actions.include?(action) }
   end
 
+  # @param [Symbol] name
+  # @param [Proc | Symbol | Object] condition
+  # @param [Object] object
+  # @param [Object] scope
+  # @param [Array<Symbol>] actions
+  # @return [true | false] Whether or not the condition matches the permission.
   def condition_matches?(name, condition, object, scope, actions)
     match = case condition
     when Proc then scope.instance_exec(object, *actions, &condition)
@@ -29,16 +46,25 @@ class Permission
     name == :unless ? !match : match && true
   end
 
+  # @param [Object] object
+  # @param [Object] scope
+  # @param [Array<Symbol>] actions
+  # @return [true | false] Whether or not all of the conditions match.
   def conditions_match?(object, scope, actions)
     conditions.empty? || conditions.all? do |name, condition|
       condition_matches?(name, condition, object, scope, actions)
     end
   end
 
+  # @return [Constant] The object as a constant.
   def object_as_constant
-    Object.const_get(object) rescue nil if object.is_a?(String)
+    return unless object.is_a?(String) || object.is_a?(Symbol)
+
+    Object.const_get(object) rescue nil
   end
 
+  # @param [Object] object
+  # @return [true | false] Whether or not the object matches.
   def object_matches?(object)
     self.object === object || object_as_constant === object
   end
--- lib/ratify/version.rb
+++ lib/ratify/version.rb
@@ -1,3 +1,4 @@
 module Ratify
+  # The gem's semantic version.
   VERSION = '0.1.0'
 end
